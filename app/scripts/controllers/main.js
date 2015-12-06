7e31b2b48504fc1a3af6747523d8874adccc18d3
Gilbert Chan <gilbert.c@gmail.com>
2015-12-06 04:17:52 -0500

    Added opacity for old data with red ring

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,7 @@ var mainControllers = angular.module('realTimeTransitApp');
 
 mainControllers.controller('MainCtrl', function ($scope, $http, $interval, NgMap) {
   $scope.vehicles = {};
-  var circles = {
-    markers: {},
-    infoWindows: {}
-  };
+  var circles = {};
 
   /*$scope.mapOptions = {
     zoom: '15',
@@ -37,9 +34,9 @@ mainControllers.controller('MainCtrl', function ($scope, $http, $interval, NgMap
     radius: 18,
     clickable: true,
     fillOpacity: 1,
-    strokeColor: '#FFFFFF',
+    strokeColor: '#CC0000',
     strokeOpacity: 0,
-    strokeWeight: 0.0001
+    strokeWeight: 2
   };
 
   var infoWindowOptions = {
@@ -82,13 +79,23 @@ mainControllers.controller('MainCtrl', function ($scope, $http, $interval, NgMap
             var newPosition = new google.maps.LatLng(parseFloat(vehicle.lat), parseFloat(vehicle.lon));
            
             var locationAge = vehicle.secsSinceReport;
-            var vehicleColor = '#000000';
-            if (locationAge >= 30) {
+            var vehicleColor = '#008900';//'#000000';
+            /*if (locationAge >= 30) {
               vehicleColor = '#CC0000';
             } else if (locationAge >= 15) {
               vehicleColor = '#000000';
             } else {
               vehicleColor = '#008900';
+            }*/
+
+            //var circleOpacity = (100 - 5 / 3 * locationAge) / 100;
+            var circleOpacity = (100 - Math.pow(1.6, locationAge/3)) / 100;
+            var strokeOpacity = 0;
+            if (circleOpacity < 0) { 
+              circleOpacity = 0;
+              strokeOpacity = 1;
+            } else if (circleOpacity < 0.2) {
+              strokeOpacity = 1;
             }
 
             //New Vehicle to add to the Map
@@ -100,26 +107,28 @@ mainControllers.controller('MainCtrl', function ($scope, $http, $interval, NgMap
                 lon: vehicle.lon,
                 /*position: [vehicle.lat, vehicle.lon],*/
                 routeId: vehicle.routeId,
-                secsSinceReport: vehicle.secsSinceReport,
+                secsSinceReport: locationAge,
                 radius: circleOptions.radius,
                 fillColor: vehicleColor,
-                fillOpacity: circleOptions.fillOpacity,
+                fillOpacity: circleOpacity, //circleOptions.fillOpacity,
                 strokeColor: circleOptions.strokeColor,
-                strokeOpacity: circleOptions.strokeOpacity,
+                strokeOpacity: strokeOpacity,
                 strokeweight: circleOptions.strokeWeight
               };
 
               //Set circle options and create circle
               circleOptions.center = newPosition;
               circleOptions.fillColor = vehicleColor;
-              circles.markers[vehicle.id] = new google.maps.Circle(circleOptions);
-              var vehicleCircle = circles.markers[vehicle.id];
-              //circles.markers[vehicle.id].setMap(map);
+              circleOptions.fillOpacity = circleOpacity;
+              circleOptions.strokeOpacity = strokeOpacity;
+              circles[vehicle.id] = new google.maps.Circle(circleOptions);
+              var vehicleCircle = circles[vehicle.id];
+              //circles[vehicle.id].setMap(map);
 
               google.maps.event.addListener(vehicleCircle, 'click', (function(vehicleCircle, vehicle) {
                 return function() {
                   //Set info window details an create
-                  infoWindow.setContent('<p><b>Route: </b> ' + vehicle.routeId + '</p><p><b>Vehicle ID:</b> ' + vehicle.id);
+                  infoWindow.setContent('<p><b>Route: </b> ' + vehicle.routeId + '</p><p><b>Vehicle ID:</b> ' + vehicle.id + '</p><b>Update :</b> ' + vehicle.secsSinceReport + '</p>');
                   infoWindow.setPosition(vehicleCircle.getCenter());
                   infoWindow.open(map);
                 };
@@ -131,35 +140,57 @@ mainControllers.controller('MainCtrl', function ($scope, $http, $interval, NgMap
               if ($scope.vehicles[vehicle.id].lat !== vehicle.lat){
                 numChanged = numChanged+1;
 
-                /*$scope.vehicles[vehicle.id].lat = vehicle.lat;
-                $scope.vehicles[vehicle.id].lon = vehicle.lon;
-                $scope.vehicles[vehicle.id].position = [vehicle.lat, vehicle.lon];
-                $scope.vehicles[vehicle.id].secsSinceReport = vehicle.secsSinceReport;
-                $scope.vehicles[vehicle.id].fillColor = '#0000FF';*/
                 $scope.vehicles[vehicle.id] = {
                   id: vehicle.id,
                   lat: vehicle.lat,
                   lon: vehicle.lon,
                   /*position: [vehicle.lat, vehicle.lon],*/
                   routeId: vehicle.routeId,
-                  secsSinceReport: vehicle.secsSinceReport,
+                  secsSinceReport: locationAge,
                   radius: circleOptions.radius,
-                  fillColor: '#0000FF',
-                  fillOpacity: circleOptions.fillOpacity,
+                  fillColor: vehicleColor,
+                  fillOpacity: circleOpacity,
                   strokeColor: circleOptions.strokeColor,
-                  strokeOpacity: circleOptions.strokeOpacity,
+                  strokeOpacity: strokeOpacity,
                   strokeweight: circleOptions.strokeWeight
                 };
 
-                circles.markers[vehicle.id].setOptions({
+                circles[vehicle.id].setOptions({
                   center: newPosition,
-                  fillColor: vehicleColor
+                  fillColor: vehicleColor,
+                  fillOpacity: circleOpacity,
+                  strokeOpacity: strokeOpacity
                 });
+
+                var vehicleCircle = circles[vehicle.id];
+
+                google.maps.event.addListener(vehicleCircle, 'click', (function(vehicleCircle, vehicle) {
+                  return function() {
+                    //Set info window details an create
+                    infoWindow.setContent('<p><b>Route: </b> ' + vehicle.routeId + '</p><p><b>Vehicle ID:</b> ' + vehicle.id + '</p><b>Update :</b> ' + vehicle.secsSinceReport + '</p>');
+                    infoWindow.setPosition(vehicleCircle.getCenter());
+                    infoWindow.open(map);
+                  };
+                })(vehicleCircle, vehicle));
+
                 //console.log('Existing Vehicle Changed: ', $scope.vehicles[vehicle.id]);
               } else {
-                circles.markers[vehicle.id].setOptions({
-                  fillColor: vehicleColor
+                circles[vehicle.id].setOptions({
+                  fillColor: vehicleColor,
+                  fillOpacity: circleOpacity,
+                  strokeOpacity: strokeOpacity
                 });
+
+                var vehicleCircle = circles[vehicle.id];
+
+                google.maps.event.addListener(vehicleCircle, 'click', (function(vehicleCircle, vehicle) {
+                  return function() {
+                    //Set info window details an create
+                    infoWindow.setContent('<p><b>Route: </b> ' + vehicle.routeId + '</p><p><b>Vehicle ID:</b> ' + vehicle.id + '</p><b>Update :</b> ' + vehicle.secsSinceReport + '</p>');
+                    infoWindow.setPosition(vehicleCircle.getCenter());
+                    infoWindow.open(map);
+                  };
+                })(vehicleCircle, vehicle));
               }
             }
           }
@@ -174,5 +205,5 @@ mainControllers.controller('MainCtrl', function ($scope, $http, $interval, NgMap
   };
   
   $scope.getAllVehicles();
-  $interval($scope.getAllVehicles, 15000);
+  $interval($scope.getAllVehicles, 5000);
 });
